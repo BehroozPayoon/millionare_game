b3410275c74b79c4a5f2e8d7b28ed6e0e105ea75
Behrooz Payoon <behrooz.payoon@gmail.com>
2024-07-01 21:53:29 +0330

    Fix refresh bug on answer result page

--- game/urls.py
+++ game/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('start/', views.start_game, name='start_game'),
-    path('play/<int:game_id>/', views.play_game, name='play_game'),
+    path('play/<int:game_id>/<int:question_index>/',
+         views.play_game, name='play_game'),
     path('results/<int:game_id>/', views.game_result, name='game_results'),
 ]
--- game/views.py
+++ game/views.py
@@ -48,11 +48,11 @@ def start_game(request):
     game.questions.set(questions)
     game.save()
 
-    return redirect('play_game', game_id=game.id)
+    return redirect('play_game', game_id=game.id, question_index=1)
 
 
 @login_required
-def play_game(request, game_id):
+def play_game(request, game_id, question_index):
     game = Game.objects.get(id=game_id)
 
     if game.completed:
@@ -63,12 +63,14 @@ def play_game(request, game_id):
         game.save()
         return redirect('game_results', game_id=game.id)
 
-    question = game.questions.all()[game.current_question - 1]
+    question = game.questions.all()[question_index - 1]
     answers = list(question.answers.all())
 
     if request.method == 'POST':
-        _process_answer(request.POST, question,
-                        game, request)
+        print(
+            f"Question Index is: {question_index} and current index is {game.current_question}")
+        return _process_answer(request.POST, question, question_index,
+                               game, request)
 
     return render(request, 'play.html', {
         'game': game,
@@ -77,17 +79,19 @@ def play_game(request, game_id):
     })
 
 
-def _process_answer(post_data, question, game, request):
+def _process_answer(post_data, question, question_index,
+                    game, request):
+
     selected_answer_id = post_data.get('answer')
     selected_answer = Answer.objects.get(id=selected_answer_id)
     correct_answer = question.answers.get(is_correct=True)
-    question_index = game.current_question
 
-    if selected_answer.is_correct:
-        game.score += question.points
+    if question_index >= game.current_question:
+        if selected_answer.is_correct:
+            game.score += question.points
 
-    game.current_question += 1
-    game.save()
+        game.current_question += 1
+        game.save()
 
     return render(request, 'answer_result.html', {
         'game': game,
